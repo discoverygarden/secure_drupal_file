691102956ed2f27e3a56c7e462dbcc2cb71c66be
lutaylor <luke@discoverygarden.ca>
2015-12-08 12:40:09 -0400

    just incase people don't update $DRUPAL_DIR less risk of destructive changes

--- secure_drupal_file.sh
+++ secure_drupal_file.sh
@@ -2,19 +2,16 @@
 DRUPAL_DIR=/var/www/drupal7
 APACHE_USER=www-data
 DRUPAL_OWNER=root
-cd $DRUPAL_DIR
-mv install.php orig.install.bak
-chown -R $DRUPAL_OWNER:$APACHE_USER .
-find . -type d -exec chmod u=rwx,g=rx,o= '{}' \;
-find . -type f -exec chmod u=rw,g=r,o= '{}' \;
-chmod 400 orig.install.bak
-cd $DRUPAL_DIR/sites
-find . -type d -name files -exec chmod ug=rwx,o= '{}' \;
-for d in ./*/files
+mv $DRUPAL_DIR/install.php $DRUPAL_DIR/orig.install.bak
+chown -R $DRUPAL_OWNER:$APACHE_USER $DRUPAL_DIR
+find $DRUPAL_DIR -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+find $DRUPAL_DIR -type f -exec chmod u=rw,g=r,o= '{}' \;
+chmod 400 $DRUPAL_DIR/orig.install.bak
+find $DRUPAL_DIR/sites -type d -name files -exec chmod ug=rwx,o= '{}' \;
+for d in $DRUPAL_DIR/sites/*/files
 do
    find $d -type d -exec chmod ug=rwx,o= '{}' \;
    find $d -type f -exec chmod ug=rw,o= '{}' \;
 done
-chmod 440 ./*/settings.php
-cd $DRUPAL_DIR
-drush cc all
+chmod 440 $DRUPAL_DIR/sites/*/settings.php
+drush -r $DRUPAL_DIR cc all
