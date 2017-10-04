d2c67ddb2f70b1897cd0d57914f998b8e6669424
Gervais <gervais@demontbrun.com>
2017-10-04 10:14:43 -0300

    Add function to autoset the apache user

--- secure_drupal_file.sh
+++ secure_drupal_file.sh
@@ -1,21 +1,67 @@
 #!/bin/bash
+
+# This script will lock down the drupal dirs and make them secure
+
+# {{{ set some constants
+
 DRUPAL_DIR=/var/www/drupal7
-APACHE_USER=www-data
 DRUPAL_OWNER=root
-if [ ! -d $DRUPAL_DIR ]; then
+
+# }}}
+# {{{ setApacheUser()
+
+setApacheUser()
+{
+  getent passwd | grep -w www-data > /dev/null
+  if [ $? -eq "0" ]; then
+    $APACHE_USER="www-data"
+  else
+	getent passwd | grep -w apache > /dev/null
+	if [ $? -eq "0" ]; then
+	  $APACHE_USER="apache"
+	fi
+  fi
+}
+
+# }}}
+# {{{ sanityCheck()
+
+sanityCheck()
+{
+  if [ ! -d $DRUPAL_DIR ]; then
    echo "DRUPAL_DIR not set properly recheck before running script"
    exit 1
-fi
-mv $DRUPAL_DIR/install.php $DRUPAL_DIR/orig.install.bak
-chown -R $DRUPAL_OWNER:$APACHE_USER $DRUPAL_DIR
-find $DRUPAL_DIR -type d -exec chmod u=rwx,g=rx,o= '{}' \;
-find $DRUPAL_DIR -type f -exec chmod u=rw,g=r,o= '{}' \;
-chmod 400 $DRUPAL_DIR/orig.install.bak
-find $DRUPAL_DIR/sites -type d -name files -exec chmod ug=rwx,o= '{}' \;
-for d in $DRUPAL_DIR/sites/*/files
-do
-   find $d -type d -exec chmod ug=rwx,o= '{}' \;
-   find $d -type f -exec chmod ug=rw,o= '{}' \;
-done
-chmod 440 $DRUPAL_DIR/sites/*/settings.php
-drush -r $DRUPAL_DIR cc all
+  fi
+
+  if [ -z $APACHE_USER ]; then
+    echo "APACHE_USER not set properly"
+	exit 1
+  fi
+}
+
+# }}}
+# {{{ SetPemissons()
+
+setPermissions()
+{
+  mv $DRUPAL_DIR/install.php $DRUPAL_DIR/orig.install.bak
+  chown -R $DRUPAL_OWNER:$APACHE_USER $DRUPAL_DIR
+  find $DRUPAL_DIR -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+  find $DRUPAL_DIR -type f -exec chmod u=rw,g=r,o= '{}' \;
+  chmod 400 $DRUPAL_DIR/orig.install.bak
+  find $DRUPAL_DIR/sites -type d -name files -exec chmod ug=rwx,o= '{}' \;
+  for d in $DRUPAL_DIR/sites/*/files
+  do
+    find $d -type d -exec chmod ug=rwx,o= '{}' \;
+    find $d -type f -exec chmod ug=rw,o= '{}' \;
+  done
+  chmod 440 $DRUPAL_DIR/sites/*/settings.php
+  drush -r $DRUPAL_DIR cc all
+}
+
+# }}}
+
+setApacheUser
+sanityCheck
+setPermissions
+
